be028ac6add52e3b812147b8c1ed32cdfa84eb34
tomgp <tom.g.pearson@gmail.com>
2015-09-15 18:00:35 +0100

    Change the orientation of the plot to emphasise what's important

--- client/components/responsive-charts/probability-table.js
+++ client/components/responsive-charts/probability-table.js
@@ -1,10 +1,12 @@
 function marketProbability(){
-	var xScale = d3.time.scale(),
-		yScale = d3.scale.linear(),
-		margins = {top:20, left:20, bottom:30, right:30 },
+	var xScale = d3.scale.linear(),
+		yScale = d3.scale.ordinal(),
+		distributionYScale = d3.scale.linear(),
+		margins = {top:60, left:0, bottom:30, right:0 },
+		gutter = 20,
 		data = [],
 		rateIncrement = 0.25,
-		dateFormat = d3.time.format('%b \'%y');
+		meetingDate = d3.time.format('%d %b %Y');
 
 	function chart(g){
 		var bounds = g.node().getBoundingClientRect(),
@@ -12,94 +14,147 @@ function marketProbability(){
 			plotHeight = bounds.height - (margins.top + margins.bottom);
 
 		var valueDomain = [0, rateIncrement * ( data[data.length-1].distribution.length )];
-		//console.log(valueDomain);
-		var dateDomain = d3.extent(data, function(d){	
-			return d.date;
-		});
+		var dateDomain = data.map(function(d){ return d.date; });
+		var meetingLabelWidth = plotWidth/3 - gutter/2;
+		var probabilityColumnWidth = 2*plotWidth/3 - gutter/2;
 
-		xScale.domain(dateDomain).range([ 0, plotWidth ]);
-		yScale.domain(valueDomain).range([ plotHeight, 0 ]);
+		xScale.domain(valueDomain).range([ 0, probabilityColumnWidth ]);
+		yScale.domain(dateDomain).rangeBands([ 0, plotHeight ]);
+		distributionYScale.domain([1,0]).range([yScale.rangeBand(), 0]);
 
-		//axes
-		var xAxis = d3.svg.axis()
-			.tickValues(data.map(function(d){ return d.date }))
-			.tickFormat(function(d){
-				return dateFormat(d);
-			})
-			.scale( xScale )
-			.orient( 'bottom' );
-
-
-		var yAxis = d3.svg.axis()
-			.scale( yScale )
-			.tickSize(plotWidth)
-			.orient( 'right' );
-
-		g.selectAll('.x.axis').data([1]).enter()
+//axis
+		g.selectAll('.axis-increment')
+			.data(d3.range(0, valueDomain[1], rateIncrement))
+				.enter()
 			.append('g')
-			.attr('class','x axis');
+				.attr('class','axis-increment')
+				.call(function(parent){
+					parent.append('text').attr('class','axis-label');
+					parent.append('line').attr('class','axis-line');
+				})
+
+		g.selectAll('.axis-label')
+			.attr({
+				y:2*margins.top/3,
+				x:function(d){
+					return plotWidth - probabilityColumnWidth + xScale(d) + xScale(rateIncrement)/2
+				},
+				'text-anchor':'middle',
+				'font-size':function(){
+							if(plotWidth>580) return '';
+							if(plotWidth>400) return '14px';
+							return '10px'
+						}
+			}).text(function(d){return d})
+
+		g.selectAll('.axis-line')
+			.attr({
+				x1:function(d){
+					return plotWidth - probabilityColumnWidth + xScale(d) + xScale(rateIncrement)/2
+				},
+				y1:margins.top,
+				x2:function(d){
+					return plotWidth - probabilityColumnWidth + xScale(d) + xScale(rateIncrement)/2
+				},
+				y2:plotHeight
+			});
 		
-		g.selectAll('.x.axis')
-			.attr('transform','translate('+margins.left+',' + (plotHeight+margins.top) + ')')
-			.call(xAxis)
-				.selectAll('text').attr('style','');
 
-		g.selectAll('.y.axis').data([1]).enter()
+//distributions
+		g.selectAll('g.distributions').data([0]).enter()
 			.append('g')
-			.attr('class','y axis');
+				.attr('class','distributions');
 
-		g.selectAll('.y.axis')
-			.attr('transform','translate('+ margins.left +', ' + margins.top + ')')
-			.call(yAxis)
-				.selectAll('text')
-				.attr('dy',0);
+		g.selectAll('g.distributions')
+			.attr('transform','translate('+(plotWidth - probabilityColumnWidth)+',0)');
 
-		//meetings (there's a probability distribution for each meeting)
-		//so attach adistribution at each apprpriate pointpoint
-		g.selectAll('.plot').data([1]).enter()
-			.append('g').attr('class','plot')
+		g.select('.distributions').selectAll('.probability-distribution')
+			.data(data)
+				.enter()
+			.append('g')
+				.attr({
+					'class':'probability-distribution',
+					'transform':function(d){
+						return 'translate(0, ' + (margins.top + yScale(d.date)) + ')';
+					}
+				})
 
-		var p = d3.selectAll('.plot');
-		p.attr('transform', function(d){
-			return 'translate('+margins.left+','+margins.top+')'
-		})
+		g.selectAll('.probability-distribution')
+			.each(function(d){
+				var parent = d3.select(this);
+				
+				var dataEnter = parent.selectAll('.probability')
+					.data(d.distribution)
+						.enter()
+				
+				dataEnter.append('rect')
+					.attr('class','probability')
 
-		p.selectAll('.probability-distribution').data(data)
-			.enter()
-				.append('g').attr({'class':'probability-distribution'});
-
-		p.selectAll('.probability-distribution').attr({
-				'transform':function(d){
-					return 'translate(' + xScale(d.date) + ',0)'
-				}
-			}).each(function(d){
-				var parent = d3.select(this)
-				parent.selectAll('.probability').data(d.distribution)
-					.enter()
-						.append('rect')
-						.attr({
-							'class':'probability',
-							'x':0,
-							'y':function(d,i){
-								return yScale((i+1)*rateIncrement);
-							},
-							'width':function(d){
-								return d * 100;
-							},
-							'height':yScale(rateIncrement) - yScale(rateIncrement*2)
+				dataEnter.append('text')
+					.attr('class','probability-label')
+						.text(function(d){
+							var rounded = Math.round(d * 10) / 10;
+							if (rounded === 0) return '';
+							return rounded;
 						});
 
+				parent.selectAll('.probability-label')
+					.attr({
+						x:function(d,i){ return xScale(i/4) + xScale(1/8); },
+						'text-anchor':'middle',
+						y:function(d){
+							return distributionYScale(1) - distributionYScale(d) - 2; 
+						},
+						'font-size':function(){
+							if(plotWidth>580) return '';
+							if(plotWidth>400) return '14px';
+							return '10px'
+						}
+					})
+
+				parent.selectAll('.probability')
+					.attr({
+						y:function(d){
+							if(distributionYScale(d)<1) return distributionYScale(1) - 1;
+							return distributionYScale(1) - distributionYScale(d); 
+						},
+						x:function(d,i){ return xScale(i/4); },
+						height:function(d){
+							if(distributionYScale(d)<1) return 1;
+							return distributionYScale(d);
+						},
+						width:xScale(0.25)
+					})
 			});
+
+
+//labels
+		g.selectAll('g.meetings').data([0]).enter()
+			.append('g')
+				.attr('class','meetings')
+
+		g.select('g.meetings')
+			.selectAll('.meeting-label')
+				.data(data.map(function(d){ return d.date; }))
+			.enter()
+				.append('text').attr('class','meeting-label');
+
+		g.selectAll('.meeting-label')
+			.attr({
+				'transform':function(d,i){
+								return 'translate(' + meetingLabelWidth + ', ' + (margins.top + yScale(d) + yScale.rangeBand()) + ')';
+							},
+				'text-anchor':'end'
+			})
+			.text(function(d){
+				return meetingDate(d);
+			})
+
 
 	}
 
 	chart.probabilityTree = function(tree){
-		data = tree.map(function(d){
-			//console.log(productSum/d.distribution.length * rateIncrement);
-			d.expectedValue = getExpectedValue( d.distribution ) * rateIncrement;
-			//console.log(d.date, d.expectedValue);
-			return d;
-		});
+		data = tree;
 		return chart;
 	}
 
